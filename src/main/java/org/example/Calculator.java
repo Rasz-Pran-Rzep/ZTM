236e9298dc1f8a7c3efe249d0f9b9ac5e3e750a4
Raszkiewicz <iraszkiewicz7@gmail.com>
2026-01-15 12:37:02 +0100

    Initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Calculator {
+    public int power(int x) {
+        return x * x;
+    }
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,26 @@
+import org.example.Calculator;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void test1(){
+        int x = 5;
+        int y = 5;
+        assert x == y;
+    }
+
+    @Test
+    void test4(){
+        Calculator cal = new Calculator();
+        int y = cal.power(5);
+        assert y == 25;
+    }
+
+    @Test
+    void test5(){
+        Calculator cal = new Calculator();
+        int y = cal.power(3);
+        assert y != 10;
+    }
+}
